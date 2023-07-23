506ab3e42869c45482f404847963eef981609306
meeran <meeran.tajalli.mt@gmail.com>
2023-07-23 18:18:30 +0500

    Fixed error where any user could view any user's friends

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :friends
-  root 'home#index'
+  #root 'home#index'
+  root 'friends#index'
   get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
